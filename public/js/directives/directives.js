8a9d5772801589452a9790bf66c835d32d22ad9c
= <ousmanefouchimi@gmail.com>
2018-02-25 02:05:03 -0700

    Used require feature with ^ to location the controller of accordion for use inside accordion-item directive

--- public/js/directives/directives.js
+++ public/js/directives/directives.js
@@ -10,4 +10,29 @@ angular.module("parking")
 	close: "&close"     
      }
    }
- });
+ })
+ .directive("accordion", function(){
+    return {
+      templateUrl: "../../views/accordion.html",
+      restrict: "E",
+      transclude: true	    
+    }
+ })
+ .directive("accordionItem", function(){
+    return {
+      templateUrl: "../../views/accordionItem.html",
+      restrict: "E",
+      scope: {
+        title: "@"
+      },
+      transclude: true,
+      require: '^accordion',	    
+      link: function(scope, element, attrs, ctrl, transcludeFn) {
+        element.bind("click", function(){
+	  scope.$apply(function() {
+	   scope.active = !scope.active;  
+	  });
+	});
+      }
+    };
+});
